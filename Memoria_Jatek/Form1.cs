0589918a1ccb30084f3024f1ab9b70a2eaafd8ec
Evbuomwan Abigel Osaretin <naotaeki@gmail.com>
2025-01-05 20:51:56 +0100

    ugyanolyan számok esélye nincs, labelbe való írás meg van, gomb helye nincs meg de indít verifikálást sikeresen

--- Memoria_Jatek/Form1.cs
+++ Memoria_Jatek/Form1.cs
@@ -16,17 +16,38 @@ namespace Memoria_Jatek
         int minrandom;
         int maxrandom;
 
+        Button verifyButton;
+
         Label[] kartyak;
         int db = 6;
         int[] randomszamok;
 
+        TextBox editBox = new TextBox();
+
         public Form1()
         {
             InitializeComponent();
+
+            editBox.Visible = false; 
+            editBox.Leave += EditBox_Leave;
+            editBox.KeyDown += EditBox_KeyDown;
+            this.Controls.Add(editBox);
+
             Kezdes();
-            GenerateMemoria();
+            GenerateMemoria(db);
             timer1.Start();
             this.Text = "Memória Játék";
+
+            verifyButton = new Button()
+            {
+                Text = "Verify",
+                Width = 100,
+                Height = 40,
+                Left = 10,
+                Top = 150
+            };
+            verifyButton.Click += VerifyButton_Click; 
+            this.Controls.Add(verifyButton);
         }
 
         public void Kerdesek()
@@ -73,23 +94,29 @@ namespace Memoria_Jatek
             }
         }
 
-        public void GenerateMemoria()
+        public void GenerateMemoria(int szam)
         {
             kartyak = new Label[db];
             int width = 85;
-            int height = 150;
+            int height = 85;
             int padding = 15;
-            int offset = width + padding;
+
+            int offsetX = width + padding;
+            int offsetY = height + padding;
+            int sorok = 3;
 
             for (int i = 0; i < db; i++)
             {
+                int oszlop = i / sorok;
+                int sor = i % sorok;
+
                 kartyak[i] = new Label()
                 {
                     Width = width,
                     Height = height,
-                    Left = i * offset + padding,
-                    Top = padding,
-                    BorderStyle = BorderStyle.Fixed3D,
+                    Left = sor * offsetX + padding,
+                    Top = oszlop * offsetY + padding,
+                    BorderStyle = BorderStyle.FixedSingle,
                     TextAlign = ContentAlignment.MiddleCenter,
                     Font = new Font("Arial", 14),
                     BackColor = Color.DarkCyan,
@@ -101,19 +128,21 @@ namespace Memoria_Jatek
                 this.Controls.Add(kartyak[i]);
             }
 
-            this.ClientSize = new Size(offset * db + padding, height + padding * 2);
+            int oszlopSzam = (int)Math.Ceiling((double)db / sorok); 
+            this.ClientSize = new Size(sorok * offsetX + padding, oszlopSzam * offsetY + padding);
         }
 
         private void Label_Click(object sender, EventArgs e)
         {
             Label clickedLabel = sender as Label;
-
-            for (int i = 0; i < db; i++){
-                if (kartyak[i] == clickedLabel)
-                {
-                    //valami valami
-                    return;
-                }
+            if (clickedLabel != null)
+            {
+                editBox.Text = clickedLabel.Text;
+                editBox.Location = clickedLabel.Location;
+                editBox.Size = clickedLabel.Size;
+                editBox.Visible = true;
+                editBox.Focus();
+                editBox.Tag = clickedLabel; // Store the clicked label in the Tag property
             }
         }
         private void timer1_Tick(object sender, EventArgs e)
@@ -142,10 +171,66 @@ namespace Memoria_Jatek
 
         public void Tolt(int mini, int maxi)
         {
-            for (int i = 0; i < randomszamok.Length; i++)
+            HashSet<int> list = new HashSet<int>();
+            while (list.Count != randomszamok.Length)
+            {   
+                list.Add( r.Next(mini, maxi));
+            }
+
+            for (int i = 0; i < list.Count; i++)
             {
-                randomszamok[i] = r.Next(mini, maxi);
+                randomszamok[i] = (list.ToArray())[i];
             }
         }
+
+        private void EditBox_Leave(object sender, EventArgs e)
+        {
+            Label label = editBox.Tag as Label;
+            if (label != null)
+            {
+                label.Text = editBox.Text;
+                editBox.Visible = false;
+            }
+        }
+
+        private void EditBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                EditBox_Leave(sender, e);
+            }
+        }
+
+        private void VerifyButton_Click(object sender, EventArgs e)
+        {
+            bool allCorrect = true;
+
+            for (int i = 0; i < db; i++)
+            {
+                if (kartyak[i].Text != kartyak[i].Name)
+                {
+                    allCorrect = false;
+                    kartyak[i].BackColor = Color.Red;
+                }
+                else
+                {
+                    kartyak[i].BackColor = Color.Green;
+                }
+            }
+
+            if (allCorrect)
+            {
+                MessageBox.Show("All labels are correct!");
+            }
+            else
+            {
+                MessageBox.Show("Some labels are incorrect. Try again!");
+            }
+        }
+
+
+        //teendő: labelbe való írás -> verifikálás, valami gomb ami indítja a verifikálást
+        //ötlet: label név: válasz (már meg van), ha input = válasz, akkor jó
+        //+ kéne még a próbálkozás counter, ha 0 akkor egyből indítja a verifikációt
     }
 }
